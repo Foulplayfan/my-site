46aae9c6019e41a61a57282cf0705ddc56ab41aa
faariz <faarizyt@gmail.com>
2025-09-21 23:15:24 -0400

    First variable created & used

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('my-site');
+  name : string = 'Angular';
 }
